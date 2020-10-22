0c7b8e0d7d5f6740d1bac292c21140e9ecf6819f
barbisliboni <niinaliboni@gmail.com>
2020-10-21 23:20:17 -0300

    Aula prática 21/10

---
+++ app/src/main/java/br/com/barbaraliboni/celleptechcurso/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.barbaraliboni.celleptechcurso
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/br/com/barbaraliboni/celleptechcurso/SplashActivity.kt
@@ -0,0 +1,11 @@
+package br.com.barbaraliboni.celleptechcurso
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
